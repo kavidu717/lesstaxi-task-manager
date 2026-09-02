1216a41069027711f4f47a0c6f21feeea18ab2e8
kavidu717 <dushmanthakavidu143@gmail.com>
2026-09-02 16:08:23 +0530

    feat:implement the user model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    role:"admin" | "user";
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [3, "Name must be at least 3 characters long"],
+        maxlength: [50, "Name must be at most 50 characters long"],
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        
+        select: false,
+    }
+    ,
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+        required: true,
+    },
+
+    
+    
+    
+}, { timestamps: true });
+
+export default mongoose.model<IUser>("User", userSchema);
+
